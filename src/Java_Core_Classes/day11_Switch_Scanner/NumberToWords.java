5d3bbc1b163434ae0101578460c043761eb803f5
canantu <67923214+canantu@users.noreply.github.com>
2022-09-27 10:00:41 +0200

    Core Java day11

---
+++ src/Java_Core_Classes/day11_Switch_Scanner/NumberToWords.java
@@ -0,0 +1,21 @@
+package Java_Core_Classes.day11_Switch_Scanner;
+
+public class NumberToWords {
+    public static void main(String[] args) {
+
+        int num =0;
+        String result = (num >= 0 && num<=9)? (num == 0)? "zero" : (num == 1)? "one" : (num == 2)? "two" :
+                (num == 3)? "three" :(num == 4)? "four" :(num == 5)? "five" :(num == 6)? "six" :(num == 7)? "seven" :
+                        (num == 1)? "eight" :"nine":"Invalid Number";
+
+        System.out.println(result);
+
+
+
+
+
+
+
+
+    }
+}
