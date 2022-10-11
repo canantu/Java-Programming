30a9e9ca5985d9062936a2717b1c8f7d0f5515ae
canantu <67923214+canantu@users.noreply.github.com>
2022-10-11 13:54:28 +0200

    day17  continue loop

---
+++ src/Java_Core_Classes/day17_While_DoWhile/BranchingStatements2.java
@@ -0,0 +1,18 @@
+package Java_Core_Classes.day17_While_DoWhile;
+
+public class BranchingStatements2 {
+
+    public static void main(String[] args) {
+
+        for (char i = 'A'; i <= 'E'; i++) {
+
+            if(i == 'C')  //to skip a character, a statement
+                continue;
+            System.out.println(i);
+
+        }
+
+
+
+    }
+}
