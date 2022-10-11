e1714b2fc9371c5d08629b768c3e46da88244b28
canantu <67923214+canantu@users.noreply.github.com>
2022-10-11 13:22:18 +0200

    day17 While Loop

---
+++ src/Java_Core_Classes/day17_While_DoWhile/DoWhileLoopIntro.java
@@ -0,0 +1,29 @@
+package Java_Core_Classes.day17_While_DoWhile;
+
+public class DoWhileLoopIntro {
+
+    public static void main(String[] args) {
+        // first execute the statement, then check the condition
+        boolean a = false;
+
+        while (a){
+            System.out.println("Wooden Spoon---while loop");
+        }
+        System.out.println("--------------------------");
+
+        do {  //JUST DO IT
+            System.out.println("Wooden Spoon---do while loop");
+        } while(a);
+        System.out.println("--------------------------");
+
+        for (int i = 0; a;) {
+            System.out.println("Wooden Spoon---for loop");
+
+        }
+
+
+
+
+
+    }
+}
