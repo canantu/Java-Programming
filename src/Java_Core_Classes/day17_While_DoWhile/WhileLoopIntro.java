c1b8950fa067a33f9438d88ad72059abf20a4b41
canantu <67923214+canantu@users.noreply.github.com>
2022-10-11 11:23:25 +0200

    day17 While Loop

---
+++ src/Java_Core_Classes/day17_While_DoWhile/WhileLoopIntro.java
@@ -0,0 +1,14 @@
+package Java_Core_Classes.day17_While_DoWhile;
+
+public class WhileLoopIntro {
+
+    public static void main(String[] args) {
+
+        String str = "Wooden Spoon";
+        int c = 0;
+        while(c<10){
+            System.out.println("str = " + str);
+            c++;
+        }
+    }
+}
