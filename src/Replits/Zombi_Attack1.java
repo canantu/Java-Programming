eb6f4ca7521d171370683ca5f00614a61063d196
canantu <67923214+canantu@users.noreply.github.com>
2022-10-17 11:10:56 +0200

    week06 replit task

--- src/Replits/Zombi_Attack1.java
+++ src/Replits/Zombi_Attack1.java
@@ -8,17 +8,22 @@ public class Zombi_Attack1 {
 
         Scanner scan = new Scanner(System.in);
         int inhabitants = scan.nextInt();
+        scan.close();
         //WRITE YOUR CODE HERE:
         int day = 0;
+        double wipeOut = 0;
 
         while(inhabitants>1){
-
             System.out.println("Day " + day + " [" + inhabitants + "]");
-            inhabitants -= inhabitants/2;
-            if(inhabitants == 1)
-                System.out.println("---EXTINCT---");
+            wipeOut = inhabitants / 2;
+            inhabitants -= (int) wipeOut;
             day++;
-
         }
+        if(inhabitants == 1){
+            System.out.println("Day " + day + " [" + inhabitants + "]");
+            System.out.println("---- EXTINCT ----");
+        }
+
+
     }
 }
