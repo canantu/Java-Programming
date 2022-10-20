650e17a2c258d94654c77aecde0411d126e4e325
canantu <67923214+canantu@users.noreply.github.com>
2022-10-20 08:24:03 +0200

    Office Hour 1

---
+++ src/EU10_Office_Hours/Task1.java
@@ -0,0 +1,10 @@
+package EU10_Office_Hours;
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        System.out.println("1\n2\n3\n123");
+
+
+    }
+}
---
+++ src/EU10_Office_Hours/Task2.java
@@ -0,0 +1,22 @@
+package EU10_Office_Hours;
+
+public class Task2 {
+    public static void main(String[] args) {
+
+        System.out.println("    10");
+        System.out.println("   / \\");
+        System.out.println("  20  30");
+        System.out.println(" / \\");
+        System.out.println("40   50");
+
+        System.out.println();
+        System.out.println("\"\"    \"\"");
+        System.out.println("  \\/");
+
+        System.out.println("   /*");
+        System.out.println("");
+        System.out.println("\"\"      \"\"");
+        System.out.println("");
+        System.out.println("   \\/");
+    }
+}
