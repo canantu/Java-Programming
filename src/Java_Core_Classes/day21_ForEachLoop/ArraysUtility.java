3e7fd737ea5d1a666c69fdd6df7a38efea5647af
canantu <67923214+canantu@users.noreply.github.com>
2022-10-25 13:11:15 +0200

    day21 Arrays Utility

---
+++ src/Java_Core_Classes/day21_ForEachLoop/ArraysUtility.java
@@ -0,0 +1,17 @@
+package Java_Core_Classes.day21_ForEachLoop;
+
+import java.util.Arrays;
+
+public class ArraysUtility {
+    public static void main(String[] args) {
+
+        int nums[] = {1,2,3,4,5};
+
+        System.out.println(nums); //hashcode
+        System.out.println(Arrays.toString(nums));
+        System.out.println(nums[0]);
+        //when we print a String, toString method called implicitly
+        System.out.println("-----------------------------------");
+
+    }
+}
