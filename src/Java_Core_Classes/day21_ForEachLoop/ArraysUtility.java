af82c12c19c880dfc0aa4c420a9a08363d712a65
canantu <67923214+canantu@users.noreply.github.com>
2022-10-25 13:36:26 +0200

    day21 Arrays Utility

--- src/Java_Core_Classes/day21_ForEachLoop/ArraysUtility.java
+++ src/Java_Core_Classes/day21_ForEachLoop/ArraysUtility.java
@@ -33,8 +33,28 @@ public class ArraysUtility {
 
         System.out.println("----------------------------------");
 
+        int[] arr1 = {1,3,2};
+        int[] arr2 = {1,2,3};
 
+        boolean r1 = Arrays.equals(arr1, arr2);
+        System.out.println(r1);
 
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        boolean r2 = Arrays.equals(arr1, arr2);
+        System.out.println(r2);
+
+        System.out.println("-----------------------------------");
+        //Anagram task, first sort arrays, them compare them if they are equal.
+        char[] ch1 = {'a','c','b'};
+        char[] ch2 = {'b','a','c'};
+
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+
+        boolean r3 = Arrays.equals(ch1, ch2);
+        System.out.println("Are they Anagram: " + r3);
 
 
 
