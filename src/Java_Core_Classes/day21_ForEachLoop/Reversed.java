fe476cae696a9da9a2c99615003815864c4326e6
canantu <67923214+canantu@users.noreply.github.com>
2022-10-26 21:37:58 +0200

    day21 For Each Practice

--- src/Java_Core_Classes/day21_ForEachLoop/Reversed.java
+++ src/Java_Core_Classes/day21_ForEachLoop/Reversed.java
@@ -1,4 +1,20 @@
 package Java_Core_Classes.day21_ForEachLoop;
 
 public class Reversed {
+
+    public static void main(String[] args) {
+
+        String[] names = {"Canan Ali", "Ali Canan", "Canan Canan", "Ali Ali"};
+
+        for (String name : names) {
+
+            String reversed = "";
+            for (int i = name.length() -1 ; i >=0 ; i--) {
+                reversed += name.charAt(i);
+
+            }
+
+            System.out.println(reversed);
+        }
+    }
 }
