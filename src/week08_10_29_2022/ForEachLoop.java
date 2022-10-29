0834ab1affd68ca989c2e0acdc4a617a843f2129
canantu <67923214+canantu@users.noreply.github.com>
2022-10-29 17:56:01 +0200

    week08 Arrays

--- src/week08_10_29_2022/ForEachLoop.java
+++ src/week08_10_29_2022/ForEachLoop.java
@@ -1,5 +1,7 @@
 package week08_10_29_2022;
 
+import java.util.Arrays;
+
 public class ForEachLoop {
 
     public static void main(String[] args) {
@@ -7,12 +9,28 @@ public class ForEachLoop {
         int[] numbers = {1,2,3,4,5,6};
 
         for (int number : numbers) {
-            System.out.println(number);
+            System.out.print(number + " ");
+        }
+        //toCharArray  String to char Array
+        System.out.println();
+        System.out.println(Arrays.toString("Adam".toCharArray()));
+        String str = "Canan";
+        char[] letters = str.toCharArray();
+
+        System.out.println(Arrays.toString(letters));
+
+        //split(regex)
+
+        String sentence = "Java is a good language.";
+
+        String[] words = sentence.split(" ");
+        for (String each: words) {
+            System.out.println(each.replace(".", ""));
         }
 
-        for(int each : numbers){
-            System.out.println(each);
-        }
+
+
+
 
 
 
