2c03d06ac18d86891a43c87df606eda30694a314
canantu <67923214+canantu@users.noreply.github.com>
2022-10-29 18:31:34 +0200

    week08 Arrays

--- src/week08_10_29_2022/FirstAndLastChar.java
+++ src/week08_10_29_2022/FirstAndLastChar.java
@@ -5,10 +5,25 @@ public class FirstAndLastChar {
     public static void main(String[] args) {
 
         String[] str = { "Anna", "Mike", "Aliya", "Donald", "Muhtar", "Remus", "Mehmet","Asya"};
-
+        int count = 0;
         for (String s : str) {
             if (s.toLowerCase().charAt(0) == s.toLowerCase().charAt(s.length() -1))
-                System.out.println(s);
+                count++;
         }
+
+        System.out.println(count);
+
+        System.out.println("=====================================");
+
+        int counter = 0;
+        for (String s : str) {
+            String first = ""+s.charAt(0);
+            String last = ""+s.charAt(s.length()-1);
+
+            if (first.equalsIgnoreCase(last))
+                counter++;
+        }
+
+        System.out.println(counter);
     }
 }
