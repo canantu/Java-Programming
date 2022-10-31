bbd9187bbcc1f0d171095877df2ac98326208f04
canantu <67923214+canantu@users.noreply.github.com>
2022-10-31 23:29:44 +0100

    day22 Multi-dimensional array

--- src/Java_Core_Classes/day22_MultiDimensionalArray/IteratingMultiDimensionalArray2.java
+++ src/Java_Core_Classes/day22_MultiDimensionalArray/IteratingMultiDimensionalArray2.java
@@ -15,14 +15,45 @@ public class IteratingMultiDimensionalArray2 {
             System.out.println();
         }
 
+
+
+        /*
+        task:
+        9 10 11 12 13
+        4 5 6 7 8
+        1 2 3
+         */
+
         System.out.println("=======================================");
 
+        /*
+        task:
+        3 2 1
+        8 7 6 5 4
+        13 12 11 10 9
+
+         */
         for (int i = 0; i < arr2D.length; i++) {
             for (int j = arr2D[i].length - 1; j >= 0; j--) {
                 System.out.print(arr2D[i][j] + " ");
             }
             System.out.println();
         }
+
+        System.out.println("------------------------------");
+
+        for (int i = arr2D.length - 1; i >= 0; i--) {
+            for (int j = arr2D[i].length - 1; j >= 0; j--) {
+                System.out.print(arr2D[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        /*
+        13 12 11 10 9
+        8 7 6 5 4
+        3 2 1
+         */
 
     }
 }
