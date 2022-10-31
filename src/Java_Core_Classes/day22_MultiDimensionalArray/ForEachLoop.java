50ea846c65df540e3dc388cfda179584e746ce7c
canantu <67923214+canantu@users.noreply.github.com>
2022-10-31 23:35:11 +0100

    day22 Multi-dimensional array

---
+++ src/Java_Core_Classes/day22_MultiDimensionalArray/ForEachLoop.java
@@ -0,0 +1,20 @@
+package Java_Core_Classes.day22_MultiDimensionalArray;
+
+import java.util.Arrays;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+
+        int[][] arr2D = {{1,2,3}, {4,5,6,7}, {8,9,10,11,12}};
+
+        for (int[] each : arr2D) {
+            System.out.println(Arrays.toString(each));
+            for (int i : each) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+
+
+    }
+}
