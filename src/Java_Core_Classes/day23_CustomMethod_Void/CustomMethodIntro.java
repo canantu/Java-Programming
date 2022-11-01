aa4b1d058a6e97e7a3eeb25a3de4668bf296db3f
canantu <67923214+canantu@users.noreply.github.com>
2022-11-01 15:46:11 +0100

    day23 Custom Method Void

---
+++ src/Java_Core_Classes/day23_CustomMethod_Void/CustomMethodIntro.java
@@ -0,0 +1,16 @@
+package Java_Core_Classes.day23_CustomMethod_Void;
+
+public class CustomMethodIntro {
+
+
+    public static void greeting() {
+
+        System.out.println("Hello World!");
+        System.out.println("I love Java");
+    }
+
+    public static void main(String[] args) {
+
+        greeting();
+    }
+}
