9def71107e53b17dd262d4deeffc89d59a1141dc
canantu <67923214+canantu@users.noreply.github.com>
2022-11-03 12:45:51 +0100

    day24 Return Methods

---
+++ src/Java_Core_Classes/day24_CustomMethod_Return/maxNumber.java
@@ -0,0 +1,20 @@
+package Java_Core_Classes.day24_CustomMethod_Return;
+//in order to import class: import packagename.classname;
+// to call method: classname.method()
+public class maxNumber {
+    public static void main(String[] args) {
+        int[] num = {1,3,5,2,9,6,4};
+        int max = max(num);
+        System.out.println("max = " + max);
+    }
+
+    public static int max(int[] num){
+        int max = num[0];
+        for (int i : num) {
+            if (max < i)
+                max = i;
+        }
+
+        return max;
+    }
+}
