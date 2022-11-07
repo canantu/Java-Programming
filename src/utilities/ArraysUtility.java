f0cac59866c198237623ea3fe69a4939b1145310
canantu <67923214+canantu@users.noreply.github.com>
2022-11-07 23:41:41 +0100

    day25 Arrays Utility Methods

--- src/utilities/ArraysUtility.java
+++ src/utilities/ArraysUtility.java
@@ -3,19 +3,19 @@ package utilities;
 public class ArraysUtility {
 
     //prints each integer element from an integer array in separate lines
-    public static void printEach(int[] arr){
+    public static void printEachElement(int[] arr){
         for (int each : arr) System.out.println(each);
     }
     //prints each String element from a String array in separate lines
-    public static void printEach(String[] arr){
+    public static void printEachElement(String[] arr){
         for (String each : arr) System.out.println(each);
     }
     //prints each double element from a double array in separate lines
-    public static void printEach(double[] arr){
+    public static void printEachElement(double[] arr){
         for (double each : arr) System.out.println(each);
     }
     //prints each char element from a char array in separate lines
-    public static void printEach(char[] arr){
+    public static void printEachElement(char[] arr){
         for (char each : arr) System.out.println(each);
     }
 }
