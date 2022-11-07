cf82d766724555a7b80152ba1f4b2bb0f08eb8ad
canantu <67923214+canantu@users.noreply.github.com>
2022-11-07 23:29:32 +0100

    day25 Custom Method - Overloading

--- src/Java_Core_Classes/day25_CustomMethod_Overloading/Test.java
+++ src/Java_Core_Classes/day25_CustomMethod_Overloading/Test.java
@@ -4,11 +4,31 @@ import utilities.StringUtility;
 
 public class Test {
     public static void main(String[] args) {
+
         String str = "Java Programming Language";
         StringUtility.printEachChar(str);
 
         String result = StringUtility.reverse(str);
         System.out.println(result);
+
+        String word = "Civic";
+        System.out.println(StringUtility.isPalindrome(word));
+
+        String word1 = "Java";
+        System.out.println(StringUtility.isPalindrome(word1));
+
+        String[] names = {"Anna", "Java", "Python", "Racecar", "Mom", "Cydeo"};
+        int count = 0;
+        for (String name : names) {
+            if (StringUtility.isPalindrome(name))
+                count++;
+        }
+        System.out.println(count);
+
+        System.out.println(StringUtility.isAnagram("baba", "abba"));
+
+
+        System.out.println(StringUtility.removeDuplicates("canan"));
 
     }
 
--- src/utilities/StringUtility.java
+++ src/utilities/StringUtility.java
@@ -1,20 +1,53 @@
 package utilities;
 
+import java.util.Arrays;
+
 public class StringUtility {
 
 
+    //prints each character of a given string
     public static void printEachChar(String str){
         for (int i = 0; i < str.length(); i++) {
             System.out.println(str.charAt(i));
         }
     }
 
+    //reverse the given string and returns reversed string
     public static String reverse(String str){
         String result = "";
         for (int i = str.length()-1; i >= 0 ; i--) {
             result += str.charAt(i);
         }
 
+        return result;
+    }
+
+    //checks if the given string is palindrome and returns boolean
+    public static boolean isPalindrome(String str){
+        return reverse(str).equalsIgnoreCase(str);
+    }
+    //checks if the given string is anagram and returns boolean
+    public static boolean isAnagram(String str1, String str2){
+
+        char[] ch1 = str1.toCharArray();
+        char[] ch2 = str2.toCharArray();
+
+        Arrays.sort(ch1);
+        Arrays.sort(ch2);
+
+        return Arrays.equals(ch1, ch2);
+    }
+
+    //removes the duplicates and return a string
+    public static String removeDuplicates(String str){
+
+        String result = "";
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (!result.contains(""+ch))
+                result += ch;
+
+        }
         return result;
     }
 
