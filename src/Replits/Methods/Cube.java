5610792ca8f844c2dd8c90c410fc71cdb7984c7b
canantu <67923214+canantu@users.noreply.github.com>
2022-11-07 09:54:17 +0100

    Replit Tasks

---
+++ src/Replits/Methods/Cube.java
@@ -0,0 +1,17 @@
+package Replits.Methods;
+
+import java.util.Scanner;
+
+public class Cube {
+    public static void cube(){
+
+        int n = new Scanner(System.in).nextInt();
+        //TYPE YOUR CODE BELOW:
+        System.out.println(n*n*n);
+    }
+
+
+    public static void main(String[] args) {
+        cube();
+    }
+}
