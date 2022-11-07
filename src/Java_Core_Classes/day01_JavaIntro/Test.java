58890deb34ae96a3c7ed7f53f93dcca23d9b593a
canantu <67923214+canantu@users.noreply.github.com>
2022-11-07 23:01:30 +0100

    day25 Custom Method - Overloading

---
+++ src/Java_Core_Classes/day01_JavaIntro/Test.java
@@ -0,0 +1,9 @@
+package Java_Core_Classes.day01_JavaIntro;
+import utilities.StringUtility; //packageName.className
+public class Test {
+    public static void main(String[] args) {
+
+        String str = "Wooden Spoon";
+        StringUtility.printEachChar(str); //can be called through class name, since method is static
+    }
+}
---
+++ src/utilities/StringUtility.java
@@ -0,0 +1,24 @@
+package utilities;
+
+public class StringUtility {
+
+
+    public static void printEachChar(String str){
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
+        }
+    }
+
+    public static String reverse(String str){
+        String result = "";
+        for (int i = str.length()-1; i >= 0 ; i--) {
+            result += str.charAt(i);
+        }
+
+        return result;
+    }
+
+
+
+
+}
