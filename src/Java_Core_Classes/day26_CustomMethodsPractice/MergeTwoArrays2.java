53f5b84908c8eaaf976b29aa84bd0da85307a074
canantu <67923214+canantu@users.noreply.github.com>
2022-11-08 21:09:52 +0100

    day26 Arrays Utility Methods

---
+++ src/Java_Core_Classes/day26_CustomMethodsPractice/MergeTwoArrays2.java
@@ -0,0 +1,28 @@
+package Java_Core_Classes.day26_CustomMethodsPractice;
+
+import utilities.ArraysUtility;
+
+import java.util.Arrays;
+
+public class MergeTwoArrays2 {
+    public static void main(String[] args) {
+        int[] arr1 = {1,2,3};
+        int[] arr2 = {4,5,6};
+        int[] arr3 = merge(arr1, arr2);
+        System.out.println(Arrays.toString(arr3));
+    }
+
+    public static int[] merge(int[] arr1, int[] arr2){
+
+        int[] result = {};
+
+        for (int each : arr1) {
+            result = ArraysUtility.addElement(result, each);
+        }
+        for (int each : arr2) {
+            result = ArraysUtility.addElement(result, each);
+        }
+
+        return result;
+    }
+}
