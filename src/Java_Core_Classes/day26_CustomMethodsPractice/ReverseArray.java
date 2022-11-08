d3b6e7428df740ad2cddf2b5b754ced94f72a5d1
canantu <67923214+canantu@users.noreply.github.com>
2022-11-08 21:34:51 +0100

    day26 Arrays Utility Methods

---
+++ src/Java_Core_Classes/day26_CustomMethodsPractice/ReverseArray.java
@@ -0,0 +1,20 @@
+package Java_Core_Classes.day26_CustomMethodsPractice;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] array = {1,2,3,4,5};
+        System.out.println(Arrays.toString(reverse(array)));
+    }
+
+    public static int[] reverse(int[] array){
+
+        int[] result = new int[array.length];
+
+        for (int i = array.length - 1, j = 0; i >= 0; i--, j++) {
+            result[j] = array[i];
+        }
+        return result;
+    }
+}
