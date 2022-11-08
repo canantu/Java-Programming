91d55231dbc70a8a221997664b20040a17597e5f
canantu <67923214+canantu@users.noreply.github.com>
2022-11-08 21:40:24 +0100

    day26 Arrays Utility Methods

--- src/Java_Core_Classes/day26_CustomMethodsPractice/ReverseArray2.java
+++ src/Java_Core_Classes/day26_CustomMethodsPractice/ReverseArray2.java
@@ -1,21 +1,23 @@
 package Java_Core_Classes.day26_CustomMethodsPractice;
 
+import utilities.ArraysUtility;
+
 import java.util.Arrays;
 
-public class ReverseArray {
+public class ReverseArray2 {
+
     public static void main(String[] args) {
         int[] array = {1,2,3,4,5};
         System.out.println(Arrays.toString(reverse(array)));
     }
-
     // reverse an array and returns a new array
     public static int[] reverse(int[] array){
+        int[] result = {};
 
-        int[] result = new int[array.length];
-
-        for (int i = array.length - 1, j = 0; i >= 0; i--, j++) {
-            result[j] = array[i];
+        for (int i = array.length - 1; i >= 0; i--) {
+            result = ArraysUtility.addElement(result, array[i]);
         }
         return result;
+
     }
 }
--- src/utilities/ArraysUtility.java
+++ src/utilities/ArraysUtility.java
@@ -338,4 +338,48 @@ public class ArraysUtility {
 
     }
 
+    // reverse the given array and returns a new array
+    public static int[] reverse(int[] array){
+        int[] result = {};
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            result = ArraysUtility.addElement(result, array[i]);
+        }
+        return result;
+
+    }
+
+    // reverse the given array and returns a new array
+    public static double[] reverse(double[] array){
+        double[] result = {};
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            result = ArraysUtility.addElement(result, array[i]);
+        }
+        return result;
+
+    }
+
+    // reverse the given array and returns a new array
+    public static char[] reverse(char[] array){
+        char[] result = {};
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            result = ArraysUtility.addElement(result, array[i]);
+        }
+        return result;
+
+    }
+
+    // reverse the given array and returns a new array
+    public static String[] reverse(String[] array){
+        String[] result = {};
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            result = ArraysUtility.addElement(result, array[i]);
+        }
+        return result;
+
+    }
+
 }
