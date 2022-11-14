fce0a468b091b60bfe08d07fa0175cd44bb0b349
canantu <67923214+canantu@users.noreply.github.com>
2022-11-14 23:23:03 +0100

    day28 Array List Practice Tasks

---
+++ src/Java_Core_Classes/day28_ArrayLists/PracticeTasks/MultiplyOddElement.java
@@ -0,0 +1,19 @@
+package Java_Core_Classes.day28_ArrayLists.PracticeTasks;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MultiplyOddElement {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.addAll(Arrays.asList(1,2,3,4,5));
+        System.out.println(list);
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i) % 2 != 0)
+                list.set(i, list.get(i)*2);
+        }
+
+        System.out.println(list);
+    }
+}
