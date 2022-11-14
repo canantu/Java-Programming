66cc12ae37ef0afae51549fe37e486d89a16c0c9
canantu <67923214+canantu@users.noreply.github.com>
2022-11-14 22:59:02 +0100

    day28 Array List

---
+++ src/Java_Core_Classes/day28_ArrayLists/RemoveDuplicates.java
@@ -0,0 +1,31 @@
+package Java_Core_Classes.day28_ArrayLists;
+
+import java.util.ArrayList;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(10);
+        list.add(10);
+        list.add(20);
+        list.add(20);
+        list.add(20);
+        list.add(30);
+        list.add(30);
+        list.add(30);
+
+        ArrayList<Integer> result = new ArrayList<>();
+        for (Integer each : list) {
+            if (result.contains(each))
+                continue;
+            result.add(each);
+        }
+
+        System.out.println(list);
+        System.out.println(result);
+
+
+    }
+}
