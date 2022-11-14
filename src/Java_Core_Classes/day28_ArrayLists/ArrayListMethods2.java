6d91f5479648f44273dcaf3ee63df5d41561b166
canantu <67923214+canantu@users.noreply.github.com>
2022-11-14 23:12:42 +0100

    day28 Array List

--- src/Java_Core_Classes/day28_ArrayLists/ArrayListMethods2.java
+++ src/Java_Core_Classes/day28_ArrayLists/ArrayListMethods2.java
@@ -1,6 +1,7 @@
 package Java_Core_Classes.day28_ArrayLists;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ArrayListMethods2 {
     public static void main(String[] args) {
@@ -48,5 +49,46 @@ public class ArrayListMethods2 {
         int b = characters.lastIndexOf('A');
         System.out.println(a);
         System.out.println(b);
+
+        System.out.println("----------------------------------------");
+
+        boolean r3 = characters.contains('A');
+        boolean r4 = characters.contains('Z');
+
+        System.out.println("r3 = " + r3);
+        System.out.println("r4 = " + r4);
+
+        System.out.println("----------------------------------------");
+
+        ArrayList<Integer> list1 = new ArrayList<>();
+        list1.add(100);
+        list1.add(100);
+        list1.add(100);
+
+        ArrayList<Integer> list2 = new ArrayList<>();
+        list2.add(100);
+        list2.add(100);
+        list2.add(100);
+
+        boolean r5 = list1.equals(list2);
+        System.out.println(list1 == list2);
+        System.out.println("r5 = " + r5);
+
+        System.out.println("----------------------------------------");
+
+        boolean r6 = list1.isEmpty();
+        System.out.println("r6 = " + r6);
+        list1.clear();
+        boolean r7 = list1.isEmpty();
+        System.out.println("r7 = " + r7);
+
+        System.out.println("----------------------------------------");
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        // bulk operation: collection type
+        numbers.addAll(Arrays.asList(1,2,3,4,5,6,7));
+        System.out.println(numbers);
+
+
     }
 }
