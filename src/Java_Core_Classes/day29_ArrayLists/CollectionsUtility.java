7dcbd9d56126b0d65c64dc882a58df9b030200fd
canantu <67923214+canantu@users.noreply.github.com>
2022-11-15 14:20:09 +0100

    day29 Array List

--- src/Java_Core_Classes/day29_ArrayLists/CollectionsUtility.java
+++ src/Java_Core_Classes/day29_ArrayLists/CollectionsUtility.java
@@ -35,6 +35,31 @@ public class CollectionsUtility {
         int min = Collections.min(list3);
         System.out.println("min = " + min);
 
+        System.out.println("--------------------------------------");
+
+        ArrayList<Integer> list4 = new ArrayList<>(Arrays.asList(10,10,20,30,40,50,10,10,10));
+
+        Collections.replaceAll(list4, 10, 100);
+        System.out.println(list4);
+        System.out.println("--------------------------------------");
+
+        int freq = Collections.frequency(list4, 1000);
+        System.out.println("freq = " + freq);
+        System.out.println("--------------------------------------");
+
+        ArrayList<String> words = new ArrayList<>(Arrays.asList("Java", "Java", "Python", "Python", "Ruby", "C#"));
+
+        int freqJava = Collections.frequency(words, "Java");
+        int freqPython = Collections.frequency(words, "Python");
+
+        System.out.println("freqJava = " + freqJava);
+        System.out.println("freqPython = " + freqPython);
+
+        boolean isFreqEqual = freqJava == freqPython;
+        System.out.println("isFreqEqual = " + isFreqEqual);
+
+
+
 
 
 
