5395bf71d227a5be633858415c03ef99c8328f96
canantu <67923214+canantu@users.noreply.github.com>
2022-11-17 00:24:32 +0100

    day31 Constructors

---
+++ src/Java_Core_Classes/day31_Constructors/RectangleObjects.java
@@ -0,0 +1,13 @@
+package Java_Core_Classes.day31_Constructors;
+
+public class RectangleObjects {
+    public static void main(String[] args) {
+
+        Rectangle rectangle1 = new Rectangle(3.5, 1.2);
+        Rectangle rectangle2 = new Rectangle(5.5, 4.2);
+
+        System.out.println(rectangle1);
+        System.out.println(rectangle2);
+
+    }
+}
