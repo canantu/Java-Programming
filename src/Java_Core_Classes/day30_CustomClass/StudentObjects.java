1710795d9f24f11da4decba8ac8b646986c989ad
canantu <67923214+canantu@users.noreply.github.com>
2022-11-16 14:46:03 +0100

    day30 Custom Classes

---
+++ src/Java_Core_Classes/day30_CustomClass/StudentObjects.java
@@ -0,0 +1,26 @@
+package Java_Core_Classes.day30_CustomClass;
+
+public class StudentObjects {
+    public static void main(String[] args) {
+
+        Student student1 = new Student();
+        student1.setInfo("Canan", 'F', 38, 123, 'A');
+        System.out.println(student1);
+
+        Student student2 = new Student();
+        student2.setInfo("Ali", 'M', 44, 124, 'B');
+
+        Student student3 = new Student();
+        student3.setInfo("Cafidan", 'F', 29, 125, 'A');
+
+        Student student4 = new Student();
+        student4.setInfo("Alisan", 'F', 27, 126, 'C');
+
+        Student student5 = new Student();
+        student5.setInfo("Alim", 'M', 50, 127, 'D');
+
+
+
+
+    }
+}
