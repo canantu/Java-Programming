375a958af4dbf2217ed8dfcbdd79c1def527b0de
canantu <67923214+canantu@users.noreply.github.com>
2022-11-18 23:42:30 +0100

    week11 warmup tasks

---
+++ src/week11_11_19_2022/warmUp/Divisible3.java
@@ -0,0 +1,21 @@
+package week11_11_19_2022.warmUp;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Divisible3 {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9));
+
+        System.out.println(divisibleBy3(list));
+    }
+
+    public static ArrayList<Integer> divisibleBy3(ArrayList<Integer> list){
+        ArrayList<Integer> divList = new ArrayList<>();
+        for (Integer each : list) {
+            if (each % 3 == 0)
+                divList.add(each);
+        }
+        return divList;
+    }
+}
