6be2e103c291a9fe962bc810235bc7761c49d0ca
canantu <67923214+canantu@users.noreply.github.com>
2022-11-19 22:21:17 +0100

    day31 practice tasks

---
+++ src/Java_Core_Classes/day31_Constructors/PracticeTasks/MainAddress.java
@@ -0,0 +1,10 @@
+package Java_Core_Classes.day31_Constructors.PracticeTasks;
+
+public class MainAddress {
+    public static void main(String[] args) {
+
+        Address address1 = new Address(23,22340, "Albert Einstein", "Berlin", "Germany");
+
+        System.out.println(address1);
+    }
+}
