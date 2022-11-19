d56b7387993c9ca2f5935dab343b48e94c291a82
canantu <67923214+canantu@users.noreply.github.com>
2022-11-19 22:25:40 +0100

    week 11 custom class

---
+++ src/Java_Core_Classes/day31_Constructors/PracticeTasks/SalaryCalculator.java
@@ -0,0 +1,56 @@
+package Java_Core_Classes.day31_Constructors.PracticeTasks;
+
+public class SalaryCalculator {
+    //hourlyRate, stateTaxRate,federalTaxRate, weeklyHours
+
+    public double hourlyRate, stateTaxRate, federalTaxRate;
+    public int weeklyHours;
+
+
+    public SalaryCalculator(double hourlyRate, double stateTaxRate, double federalTaxRate, int weeklyHours) {
+        this.hourlyRate = hourlyRate;
+        this.stateTaxRate = stateTaxRate;
+        this.federalTaxRate = federalTaxRate;
+        this.weeklyHours = weeklyHours;
+    }
+
+    public String toString() {
+        return "SalaryCalculator{" +
+                "hourlyRate=" + hourlyRate +
+                ", stateTaxRate=" + stateTaxRate +
+                ", federalTaxRate=" + federalTaxRate +
+                ", weeklyHours=" + weeklyHours +
+                '}';
+    }
+
+
+    public double salary(){
+        return hourlyRate * weeklyHours * 52;
+    }
+
+    public double stateTax(){
+        return salary()*stateTaxRate;
+    }
+
+    public double federalTax(){
+        return salary()*federalTaxRate;
+    }
+
+    public double salaryAfterTax(){
+        return salary() - federalTax() - stateTax();
+    }
+
+
+}
+/*
+salary(): calculates the salary (
+hourlyRate * weeklyHour * 52)
+            stateTax(): calculates the
+totalStateTax
+            federalTax(): calculates the
+total federal tax
+            salaryAfterTax(): calculates the
+salary after tax
+            toString(): displays the salary,
+stateTax, federalTax, salaryAfterTax of the Object
+ */
