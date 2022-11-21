82f0dc0e935bcd3f63e400e724368bbfd4bac19b
canantu <67923214+canantu@users.noreply.github.com>
2022-11-21 12:24:02 +0100

    day33 Statics

---
+++ src/Java_Core_Classes/day33_Statics/StudentTask.java
@@ -0,0 +1,17 @@
+package Java_Core_Classes.day33_Statics;
+
+//multiple classes in one java file (not recommended indeed)
+
+//custom class is defined without access modifier, can not be named as file name
+class StudentClass{ // all the other classes have default access modifier
+
+}
+
+class Test{ //there can not be more than one public class
+
+}
+
+public class StudentTask { //only one public class and name has to match with file name
+
+
+}
