fe81345e08c5a5ad907d2f3b2bdb6c8c850c9a47
canantu <67923214+canantu@users.noreply.github.com>
2022-11-23 00:03:41 +0100

    day34 Garbage Collection- Access Modifiers

---
+++ src/Java_Core_Classes/day34_GarbageCollection_AccessModifiers/Test.java
@@ -0,0 +1,20 @@
+package Java_Core_Classes.day34_GarbageCollection_AccessModifiers;
+
+import static utilities.MathUtility.*;
+public class Test {
+
+    public static void main(String[] args) {
+
+        int r1 = sum(10, 20);
+        System.out.println(r1);
+
+        int r2 = subtract(20, 10);
+        System.out.println(r2);
+
+        int r3 = max(102, 291);
+        System.out.println(r3);
+
+
+
+    }
+}
