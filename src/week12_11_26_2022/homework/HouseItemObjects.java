2a3a742cb52a8d4751f994ac52007464412f2e1b
canantu <67923214+canantu@users.noreply.github.com>
2022-11-27 20:45:08 +0100

    week 12 Constructor Overloading Review

---
+++ src/week12_11_26_2022/homework/HouseItemObjects.java
@@ -0,0 +1,13 @@
+package week12_11_26_2022.homework;
+
+public class HouseItemObjects {
+    public static void main(String[] args) {
+
+        HouseItems item1 = new HouseItems();
+        item1.name = "Table";
+        item1.color = "brown";
+        item1.amount = 1;
+
+        System.out.println(item1);
+    }
+}
---
+++ src/week12_11_26_2022/homework/HouseItems.java
@@ -0,0 +1,16 @@
+package week12_11_26_2022.homework;
+
+public class HouseItems {
+
+    public String name;
+    public String color;
+    public int amount;
+
+    public String toString() {
+        return "HouseItems{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
+}
