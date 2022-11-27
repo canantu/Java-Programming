8fdd5884da207a42fd3ecae1d05fac92e28c627f
canantu <67923214+canantu@users.noreply.github.com>
2022-11-27 20:45:53 +0100

    week 12 Statics Review

--- src/week12_11_26_2022/employee/EmployeeTest.java
+++ src/week12_11_26_2022/employee/EmployeeTest.java
@@ -4,7 +4,12 @@ public class EmployeeTest {
     public static void main(String[] args) {
 
         Employee employee1 = new Employee("Adam", 123, 'M');
-
         System.out.println(employee1);
+
+        Employee employee2 = new Employee("Eve");
+        System.out.println(employee2);
+
+        Employee employee3 = new Employee("AdamEve", 124);
+        System.out.println(employee3);
     }
 }
