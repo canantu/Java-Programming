51e5463c4d6b0f90fb91133e154ba9353d592125
canantu <67923214+canantu@users.noreply.github.com>
2022-11-28 12:56:22 +0100

    day 35 Encapsulation

---
+++ src/Java_Core_Classes/day35_Encapsulation/PersonObject.java
@@ -0,0 +1,11 @@
+package Java_Core_Classes.day35_Encapsulation;
+
+public class PersonObject {
+    public static void main(String[] args) {
+        Person person1 = new Person("Canan", 25, 'F', "Turkish");
+
+        person1.eat();
+        person1.drink();
+        System.out.println(person1);
+    }
+}
