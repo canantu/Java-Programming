c8906ff606159ddf98b11ce6d8e85df86faec2b8
canantu <67923214+canantu@users.noreply.github.com>
2022-12-15 11:45:54 +0100

    day39 Recap

--- src/Java_Core_Classes/day39_Inheritance_Encapsulation/ShapeTask/Circle.java
+++ src/Java_Core_Classes/day39_Inheritance_Encapsulation/ShapeTask/Circle.java
@@ -1,4 +1,44 @@
 package Java_Core_Classes.day39_Inheritance_Encapsulation.ShapeTask;
 
-public class Circle {
+public class Circle extends Shape{
+
+    private double radius;
+    private static double pi = 3.14;
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        if (radius<=0) {
+            System.err.println("Radius can not be 0 or less than 0");
+            System.exit(1);
+        }
+        this.radius = radius;
+    }
+
+    public Circle(double radius) {
+        super("Circle");
+        setRadius(radius);
+    }
+
+    @Override
+    public double area() {
+        return pi*radius*radius;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2*pi*radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "name= 'Circle'" +
+                ", radius=" + radius +
+                ", area='" + area() + '\'' +
+                ", perimeter='" + perimeter() + '\'' +
+                '}';
+    }
 }
