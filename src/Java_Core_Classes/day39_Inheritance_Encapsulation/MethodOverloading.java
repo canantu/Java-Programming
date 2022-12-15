7ff290628b4f83089e3e44782d44a811dfef051d
canantu <67923214+canantu@users.noreply.github.com>
2022-12-15 11:08:57 +0100

    day39 Recap

---
+++ src/Java_Core_Classes/day39_Inheritance_Encapsulation/MethodOverloading.java
@@ -0,0 +1,20 @@
+package Java_Core_Classes.day39_Inheritance_Encapsulation;
+
+public class MethodOverloading {
+    public void method1(){
+
+    }
+
+    private final void method1(String a){
+
+    }
+
+    private int method1(int a){
+        return a;
+    }
+
+
+
+
+
+}
