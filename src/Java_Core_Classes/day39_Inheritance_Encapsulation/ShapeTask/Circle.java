c85a56ad1842fef626803c884e927aa3fcf0a391
canantu <67923214+canantu@users.noreply.github.com>
2022-12-15 11:50:54 +0100

    day39 Recap

--- src/Java_Core_Classes/day39_Inheritance_Encapsulation/ShapeTask/Circle.java
+++ src/Java_Core_Classes/day39_Inheritance_Encapsulation/ShapeTask/Circle.java
@@ -3,7 +3,7 @@ package Java_Core_Classes.day39_Inheritance_Encapsulation.ShapeTask;
 public class Circle extends Shape{
 
     private double radius;
-    private static double pi = 3.14;
+    public static double pi = 3.14;
 
     public double getRadius() {
         return radius;
