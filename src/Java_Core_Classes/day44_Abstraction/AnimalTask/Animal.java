ec9f736bf87f988e2a25fcaf611720b01bc120dd
canantu <tunc.canan84@gmail.com>
2023-01-02 23:38:54 +0100

    day44 Abstraction

---
+++ src/Java_Core_Classes/day44_Abstraction/AnimalTask/Animal.java
@@ -0,0 +1,92 @@
+package Java_Core_Classes.day44_Abstraction.AnimalTask;
+
+public abstract class Animal {
+
+    private String name;
+    private final String breed;
+    private final char gender;
+    private int age;
+    private String size;
+    private final String color;
+
+    public static final boolean canBreath;
+    static {
+        canBreath = true;
+    }
+
+    public Animal(String name, String breed, char gender, int age, String size, String color) {
+        setName(name);
+        this.breed = breed;
+
+        if (!(gender == 'M' || gender == 'F')){
+            throw new RuntimeException("Invalid gender:" + gender);
+        }
+        this.gender = gender;
+
+        setAge(age);
+        setSize(size);
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name.isEmpty())
+            throw new RuntimeException("Invalid Name!");
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age <= 0)
+            throw new RuntimeException("Invalid Age!");
+        this.age = age;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        if (size.equalsIgnoreCase("small") || size.equalsIgnoreCase("large"))
+            throw new RuntimeException("Invalid size!");
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+
+
+    public abstract void eat();
+
+    public final void drink(){
+        System.out.println(name + " is drinking water");
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "{" +
+                "name='" + name + '\'' +
+                ", breed='" + breed + '\'' +
+                ", gender=" + gender +
+                ", age=" + age +
+                ", size='" + size + '\'' +
+                ", color='" + color + '\'' +
+                '}';
+    }
+}
